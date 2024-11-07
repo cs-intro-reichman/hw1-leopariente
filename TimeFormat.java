7978882c210697c1563612dba642460f1a55dae0
Leo Pariente <leopariente@gmail.com>
2024-11-07 11:12:01 +0200

    fix

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,7 +3,7 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		String suffix = hours >= 12 ? "PM" : "AM";
-		hours = hours % 12;
+		hours = hours > 12 ? hours - 12 : hours;
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 		String formattedNumber = minutes < 10 ? "0" + minutes : Integer.toString(minutes);
